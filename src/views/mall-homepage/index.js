4db4d671d3018fefaaae6200fa296ed13ab4c922
huwei <337502348@qq.com>
2019-01-02 09:36:50 +0800

    a

--- src/views/mall-homepage/index.js
+++ src/views/mall-homepage/index.js
@@ -1,18 +1,47 @@
 import "@/asset/sass/views/mall-homepage";
 // import { TabBar } from "antd-mobile";
 import React from "react";
+
+import { Tabs, WhiteSpace } from "antd-mobile";
+
 class HomePage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedTab: "redTab",
-      hidden: false,
-      fullScreen: false
-    };
-  }
+  renderContent = tab => (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "150px",
+        backgroundColor: "#fff"
+      }}
+    >
+      <p>Content of {tab.title}</p>
+    </div>
+  );
 
   render() {
-    return <div>1</div>;
+    const tabs = [
+      { title: "1st Tab" },
+      { title: "2nd Tab" },
+      { title: "3rd Tab" },
+      { title: "4th Tab" },
+      { title: "5th Tab" },
+      { title: "6th Tab" },
+      { title: "7th Tab" },
+      { title: "8th Tab" },
+      { title: "9th Tab" }
+    ];
+
+    return (
+      <div>
+        <Tabs
+          tabs={tabs}
+          renderTabBar={props => <Tabs.DefaultTabBar {...props} page={3} />}
+        >
+          {this.renderContent}
+        </Tabs>
+      </div>
+    );
   }
 }
 
