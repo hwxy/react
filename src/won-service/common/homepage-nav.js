65fb42320727acdd42cf50cfc7e0696bb71dda90
huwei <337502348@qq.com>
2019-01-17 12:24:44 +0800

    no message

--- src/won-service/common/homepage-nav.js
+++ src/won-service/common/homepage-nav.js
@@ -1,23 +1,24 @@
 import React from "react";
 import "@/asset/sass/won-service/common/homepage-nav.module.scss";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 class HomePageNav extends React.Component {
   render() {
     return (
       <div styleName="homepage-nav">
         <div styleName="nav">
-          <Link to="/" styleName="nav__icon">
-            首页
-          </Link>
-          <Link to="/icon" styleName="nav__icon">
+          <NavLink to="/" styleName="nav__icon">
+            <span className="icon-12 fz30" />
+            <span className="fz20">首页</span>
+          </NavLink>
+          <NavLink to="/icon" styleName="nav__icon">
             购物车
-          </Link>
-          <Link to="/login" styleName="nav__icon">
+          </NavLink>
+          <NavLink to="/login" styleName="nav__icon">
             登录
-          </Link>
-          <Link to="/icon" styleName="nav__icon">
+          </NavLink>
+          <NavLink to="/icon" styleName="nav__icon">
             图标
-          </Link>
+          </NavLink>
         </div>
       </div>
     );
