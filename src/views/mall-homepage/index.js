f1d06954fd8a42a54027f04f50e499f638d097fa
huwei <337502348@qq.com>
2019-01-17 15:36:18 +0800

    no message

--- src/views/mall-homepage/index.js
+++ src/views/mall-homepage/index.js
@@ -1,12 +1,12 @@
 import "@/asset/sass/views/mall-homepage.module.scss";
 import React from "react";
-import HomepageNav from "won-common/homepage-nav";
+import Navigation from "won-common/navigation";
 class HomePage extends React.Component {
   render() {
     return (
       <div>
         商城
-        <HomepageNav />
+        <Navigation {...this.props} />
       </div>
     );
   }
