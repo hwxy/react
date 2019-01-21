b3fdfda10dbe7ea3f352294c737526d087922713
huwei <337502348@qq.com>
2019-01-21 15:15:28 +0800

    no message

--- src/views/mall-homepage/index.js
+++ src/views/mall-homepage/index.js
@@ -1,11 +1,13 @@
 import "@/asset/sass/views/mall-homepage.module.scss";
 import React from "react";
+import { InputItem } from "antd-mobile";
 import Navigation from "won-common/navigation";
 class HomePage extends React.Component {
   render() {
     return (
       <div>
         商城
+        <InputItem />
         <Navigation {...this.props} />
       </div>
     );
