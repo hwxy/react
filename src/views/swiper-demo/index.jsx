0e4d78d229753b029a96ca557bdd0a15feaa5c0d
amaris <amarisyu@sina.com>
2019-01-21 19:12:36 +0800

    slider global css

--- src/views/swiper-demo/index.jsx
+++ src/views/swiper-demo/index.jsx
@@ -1,15 +1,22 @@
 import React from "react";
-import "@/asset/sass/views/swiper-demo.module.scss";
+import styles from "@/asset/sass/views/swiper-demo.module.scss";
+// import commonStyles from "../../asset/sass/common/normalize.scss";
 import Carousel from "rmc-nuka-carousel";
 
 class SwiperDemo extends React.Component {
   render() {
     return (
-      <div styleName="swiper-page">
-        <Carousel>
-          <div styleName="swiper-block" />
-          <div styleName="swiper-block" />
-          <div styleName="swiper-block" />
+      <div className="swiper-page">
+        <Carousel autoplay={true} wrapAround={true}>
+          <div className={styles["swiper-block"]}>
+            <span>111</span>
+          </div>
+          <div className={styles["swiper-block"]}>
+            <span>222</span>
+          </div>
+          <div className={styles["swiper-block"]}>
+            <span>333</span>
+          </div>
         </Carousel>
       </div>
     );
