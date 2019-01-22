3544a03ea875fa0aa873bc2bba181f4f232978a9
Unknown <amarisyu@sina.com>
2019-01-22 14:44:57 +0800

    api

--- src/api/swiperDemoApi.js
+++ src/api/swiperDemoApi.js
@@ -1,3 +1,7 @@
-import axios from "../core/network/index.js";
+import { apiGet } from "../core/network/index.js";
 const BaseUrl =
   " https://www.easy-mock.com/mock/5c3c047937e0172bd028d180/example";
+
+export function getSwiperData(params) {
+  return apiGet(`${BaseUrl}/swiperdata`);
+}
--- src/views/swiper-demo/index.jsx
+++ src/views/swiper-demo/index.jsx
@@ -2,21 +2,45 @@ import React from "react";
 import styles from "@/asset/sass/views/swiper-demo.module.scss";
 // import commonStyles from "../../asset/sass/common/normalize.scss";
 import Carousel from "rmc-nuka-carousel";
+import { getSwiperData } from "../../api/swiperDemoApi.js";
 
 class SwiperDemo extends React.Component {
+  state = {
+    swiperList: ["#fff", "#000"]
+  };
+  componentDidMount() {
+    this.getSwiperData();
+  }
+
+  getSwiperData = () => {
+    getSwiperData().then(res => {
+      this.setState({
+        swiperList: res.data
+      });
+    });
+  };
   render() {
     return (
       <div className="swiper-page">
         <Carousel autoplay={true} wrapAround={true}>
-          <div className={styles["swiper-block"]}>
-            <span>111</span>
-          </div>
-          <div className={styles["swiper-block"]}>
+          {this.state.swiperList.map((item, index) => {
+            return (
+              <div
+                className={styles["swiper-block"]}
+                style={{ backgroundColor: item }}
+                key={index}
+              >
+                <span>111</span>
+              </div>
+            );
+          })}
+
+          {/* <div className={styles["swiper-block"]}>
             <span>222</span>
           </div>
           <div className={styles["swiper-block"]}>
             <span>333</span>
-          </div>
+          </div> */}
         </Carousel>
       </div>
     );
