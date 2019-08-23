399b3a023af14ee528a49fb0fbc96fd6cd8e5b8d
huwei <337502348@qq.com>
2019-08-23 10:58:07 +0800

    no message

--- src/api/swiperDemoApi.js
+++
@@ -1,7 +0,0 @@
-import { apiGet } from "../core/network/index.js";
-const BaseUrl =
-  " https://www.easy-mock.com/mock/5c3c047937e0172bd028d180/example";
-
-export function getSwiperData(params) {
-  return apiGet(`${BaseUrl}/swiperdata`);
-}
