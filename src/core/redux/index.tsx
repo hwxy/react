a40337a5551ca245e39943da977b8dd97c1f2dc2
huwei <337502348@qq.com>
2019-09-04 09:35:10 +0800

    no message

--- src/core/redux/index.tsx
+++ src/core/redux/index.tsx
@@ -8,7 +8,7 @@ reducerFile.keys().forEach((item: string): void => {
     let value: object = reducerFile(item).default;
     Object.assign(reducers, value);
 })
-
+     
 let middlware: any[] = [thunk];
 
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
